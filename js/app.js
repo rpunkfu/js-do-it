5a51f335fca154c9f7cae084f0350ccf2b7e2b8d
rpawfuiml <oskar.cieslik@gmail.com>
2015-07-27 17:35:49 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $.fn.jsDoIt = function (options) {
+        var settings = $.extend({
+            url: ""
+        }, options);
+
+        return this.each(function () {
+
+        });
+    };
+}(jQuery));
